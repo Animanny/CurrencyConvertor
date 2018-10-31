a015165c8ed13a097bbebde73116d2e73f825b67
Ani Srikanth <ani.srikanth23@gmail.com>
2018-10-30 21:08:45 -0400

    Init - Loading Currencies Works

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+  // set endpoint and your access key
+  endpoint = 'symbols'
+  access_key = '1972a0e4b17f2acb6b1fc1e053eb13a2';
+
+  // get the most recent exchange rates via the "latest" endpoint:
+  $.ajax({
+    url: 'http://data.fixer.io/api/' + endpoint + '?access_key=' + access_key,
+    dataType: 'jsonp',
+    success: function(json) {
+      var currencySymbols = json.symbols;
+      // exchange rata data is stored in json.rates
+
+      for (var key in currencySymbols) {
+         $('#from').append("<option value='" + currencySymbols[key] + "'> " + currencySymbols[key] + " </option>")
+         $('#to').append("<option value='" + currencySymbols[key] + "'> " + currencySymbols[key] + " </option>")
+        //alert(currencySymbols[key]);
+
+      }
+
+    }
+  });
+
+
+
+  $("#convert_button").click(function() {
+    alert("Hello!");
+  });
+
+
+
+
+
+
+
+
+
+
+
+});

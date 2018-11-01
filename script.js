4381f5a4c6943511d2dea56dd9d7bbd119211208
Ani Srikanth <ani@Anis-MacBook-Pro.local>
2018-11-01 15:28:49 -0400

    Replaced API with another one cuz the first one was apparently sketchy.

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
-
   // set endpoint and your access key
-  endpoint = 'symbols'
-  access_key = '1972a0e4b17f2acb6b1fc1e053eb13a2';
 
   // get the most recent exchange rates via the "latest" endpoint:
   $.ajax({
-    url: 'http://data.fixer.io/api/' + endpoint + '?access_key=' + access_key,
-    dataType: 'jsonp',
+    url: 'https://api.exchangeratesapi.io/latest?base=USD',
+    dataType: 'json',
     success: function(json) {
-      var currencySymbols = json.symbols;
+      var currencySymbols = json.rates;
       // exchange rata data is stored in json.rates
 
       for (var key in currencySymbols) {

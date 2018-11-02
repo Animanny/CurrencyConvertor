7d92c58fd9a28162c05554583fdd9c33d4701312
Ani Srikanth <ani@Anis-MacBook-Pro.local>
2018-11-02 15:58:13 -0400

    Made it so the values will update when the currency is changed. Also CSS

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ $(document).ready(function() {
 
   $('#fromnum').on("input", function() {
     if ($("#from").children("option").filter(":selected").text() === "Choose..." || $("#to").children("option").filter(":selected").text() === "Choose...") {
-
     } else {
       var from = $("#from").children("option").filter(":selected").text();
       var to = $("#to").children("option").filter(":selected").text();
@@ -39,12 +38,90 @@ $(document).ready(function() {
           var out = amount / call[from];
 
           out = out * call[to]
-          $('#tonum').val(Math.round(out*100)/100);
+          $('#tonum').val(Math.round(out * 100) / 100);
 
         }
       });
     }
-
-
   });
+
+  $('#tonum').on("input", function() {
+    if ($("#from").children("option").filter(":selected").text() === "Choose..." || $("#to").children("option").filter(":selected").text() === "Choose...") {
+    } else {
+      var from = $("#to").children("option").filter(":selected").text();
+      var to = $("#from").children("option").filter(":selected").text();
+      var amount = $('#tonum').val();
+      var out = 0;
+
+      var endpoint = 'convert';
+      $.ajax({
+        url: 'https://api.exchangeratesapi.io/latest?base=USD',
+        dataType: 'json',
+        success: function(json) {
+          var call = json.rates;
+
+          var out = amount / call[from];
+
+          out = out * call[to]
+          $('#fromnum').val(Math.round(out * 100) / 100);
+
+        }
+      });
+    }
+  });
+
+  $('#from').change(function() {
+    if ($("#from").children("option").filter(":selected").text() === "Choose..." || $("#to").children("option").filter(":selected").text() === "Choose...") {
+    } else {
+      var from = $("#from").children("option").filter(":selected").text();
+      var to = $("#to").children("option").filter(":selected").text();
+      var amount = $('#fromnum').val();
+      var out = 0;
+
+      var endpoint = 'convert';
+      $.ajax({
+        url: 'https://api.exchangeratesapi.io/latest?base=USD',
+        dataType: 'json',
+        success: function(json) {
+          var call = json.rates;
+
+          var out = amount / call[from];
+
+          out = out * call[to]
+          $('#tonum').val(Math.round(out * 100) / 100);
+
+        }
+      });
+    }
+  });
+  $('#to').change(function() {
+    if ($("#from").children("option").filter(":selected").text() === "Choose..." || $("#to").children("option").filter(":selected").text() === "Choose...") {
+    } else {
+      var from = $("#from").children("option").filter(":selected").text();
+      var to = $("#to").children("option").filter(":selected").text();
+      var amount = $('#fromnum').val();
+      var out = 0;
+
+      var endpoint = 'convert';
+      $.ajax({
+        url: 'https://api.exchangeratesapi.io/latest?base=USD',
+        dataType: 'json',
+        success: function(json) {
+          var call = json.rates;
+
+          var out = amount / call[from];
+
+          out = out * call[to]
+          $('#tonum').val(Math.round(out * 100) / 100);
+
+        }
+      });
+    }
+  });
+
+
+
+
+
+
 });
